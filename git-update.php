fcb99c86eb48b806328072d13d19cee7d8168f48
sudipgit <sudipcseku@gmail.com>
2025-10-13 11:12:31 +0600

    Update 10

--- git-update.php
+++ git-update.php
@@ -2,13 +2,14 @@
 /**
  * Plugin Name: Git Update
  * Description: Example plugin with direct GitHub updates (no external libraries).
- * Version: 1.3.0
+ * Version: 1.3.1
  * Author: Sudip
  * Plugin URI: https://github.com/sudipgit/git-update
  */
  
  
 /* Change Log:
+    v1.3.1    ccdv  sdfgsg  d tgrhb 
     v1.3.0    ccdv  sdfgsg tgrhb 
     v1.2.9    ccdv  sdfgsrt
     v1.2.8    ccv  sdfgs tr
